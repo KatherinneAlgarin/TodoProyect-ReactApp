43cd13aa85132c3450e34b4c7d197753a76dad25
Katherinne.Algarin <katherinne.cruz1@catolica.edu.sv>
2026-02-06 17:35:19 -0600

    Cambio de version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {render} from 'react-dom';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+render(<App />, container);
